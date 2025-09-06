4425da7138444064d22596179e012019eb6a1ad8
Jo Bergantine <studio@kinsa.cc>
2025-09-06 16:29:43 +0100

    Add `eslint-plugin-tailwindcss` and configure ESLint rules for Tailwind integration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const tailwind = require('eslint-plugin-tailwindcss');
 
 module.exports = defineConfig([
   expoConfig,
@@ -12,10 +13,15 @@ module.exports = defineConfig([
     plugins: {
       prettier: require('eslint-plugin-prettier'),
       '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      tailwindcss: tailwind,
     },
     rules: {
       // Prettier integration
       'prettier/prettier': 'error',
+
+      // Disable Tailwind classnames-order since PyCharm handles it
+      'tailwindcss/classnames-order': 'off',
+      'tailwindcss/no-custom-classname': 'off',
 
       // Unused imports - TypeScript projects
       '@typescript-eslint/no-unused-vars': [
